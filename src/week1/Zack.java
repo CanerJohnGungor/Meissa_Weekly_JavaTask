c7e531c5269eb8a9ee6fe254e30b7e591f0ce52a
zarslan <arslanzekeriya0@gmail.com>
2023-05-25 12:36:59 -0400

    Day1 Third Commit

--- src/week1/Zack.java
+++ src/week1/Zack.java
@@ -4,6 +4,6 @@ public class Zack  {
     public static void main(String[] args) {
 
         System.out.println("Hello Meissa");
-        System.out.println("Have a happy week!!");
+        System.out.println("Have a happy week!!!");
     }
 }
