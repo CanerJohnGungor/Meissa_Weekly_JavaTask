6c8988f429bf5d88ed27f2c4242cb5506f6737f0
canany <66906676+canany@users.noreply.github.com>
2023-05-25 12:22:35 -0400

    Day1 - Hello from CananDay01 Branch

--- src/week1/Canan.java
+++ src/week1/Canan.java
@@ -3,5 +3,6 @@ package week1;
 public class Canan {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Hello from new CananDay01 Branch");
     }
 }
