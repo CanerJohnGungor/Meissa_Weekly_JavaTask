0e696860160197dc997640ac3b9c4baa0df553d8
zarslan <arslanzekeriya0@gmail.com>
2023-05-25 13:38:34 -0400

    Week1_Tasks

--- src/week1/Zack.java
+++ src/week1/Zack.java
@@ -1,9 +1,73 @@
 package week1;
 
 public class Zack  {
+
+    public static boolean IsEven(int number){
+        return number%2==0 ? true : false;
+    }
+
+    public static boolean isOdd (int number){
+        return number%2 !=0 ? true: false;
+    }
+
+
+    public static String isOddOrEven (int number) {
+        if (number % 2 == 0) {
+            return number + " is even number";
+        } else {
+            return number + " is odd number";
+        }
+    }
+
+
+
+    public static int divideNumber(int number,int number2) {
+        int thirdNumber = 0;
+
+        if(number2==0){
+            System.err.println(number2+" can not be 0");
+            System.exit(1);
+        }
+        while (number >= number2) {
+            number -= number2;
+            thirdNumber++;
+        }
+        return thirdNumber;
+    }
+
+    public static void finRa() {
+        String result = "";
+        for (int i = 1; i < 31; i++) {
+            if (i % 3 == 0 && i % 5 ==0) {
+                result += "FINRA ";
+            } else if (i % 5 == 0) {
+                result += "RA ";
+            } else if (i % 3 == 0) {
+                result += "FIN ";
+            } else {
+                result += i + " ";
+            }
+        }
+        System.out.println(result);
+    }
+
+
     public static void main(String[] args) {
 
-        System.out.println("Hello Meissa");
-        System.out.println("Have a happy week!!!");
+        System.out.println(IsEven(10));
+        System.out.println(isOdd(20));
+        System.out.println(isOddOrEven(25));
+
+        System.out.println(divideNumber(10, 2));
+
+        finRa();
+
+
+
+
+
+
+
     }
+
 }
