c9460bf6fd19dece7a08855b4c7d1936bcf8a942
lussadecembre <lussa29@mail.com>
2023-05-25 17:20:22 -0400

    Day 2 completed test

--- src/week1/Lussa.java
+++ src/week1/Lussa.java
@@ -4,5 +4,6 @@ public class Lussa {
     public static void main(String[] args) {
         System.out.println("Hello Friends");
         System.out.println("Have a happy week");
+        System.out.println("New test");
     }
 }
