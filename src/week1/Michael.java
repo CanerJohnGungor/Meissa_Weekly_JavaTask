59e0797693339484ad6ffc90153d83d26f933b80
Ersin KAYHAN <dekked2438@gmail.com>
2023-05-25 19:52:08 +0300

    Day2 First Commit

--- src/week1/Michael.java
+++ src/week1/Michael.java
@@ -4,5 +4,6 @@ public class Michael {
     public static void main(String[] args) {
         System.out.println("Hello friends");
         System.out.println("Have a happy week!!!");
+        System.out.println("This is the second day.");
     }
 }
