03b9272276b206c52de62ef6993b53f5b3c93768
flittle85 <flittle85@gmail.com>
2023-05-27 11:49:40 -0400

    day3 completed tests

--- src/week2/falana.java
+++ src/week2/falana.java
@@ -1,9 +1,12 @@
-public class falana_java_class {
+package week2;
+
+public class falana {
 
     public static void main(String[] args) {
         System.out.println("hello friends");
 
         System.out.println("Hello again");
+        System.out.println("Hello");
     }
 
 }
