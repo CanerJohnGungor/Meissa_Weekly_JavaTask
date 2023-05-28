f55ad82576871b799c9c80f067e8d0da8cdce536
lussadecembre <lussa29@mail.com>
2023-05-28 08:15:29 -0400

    day 4 comment

--- src/week2/Lussa_Day_2.java
+++ src/week2/Lussa_Day_2.java
@@ -2,6 +2,8 @@ package week2;
 
 public class Lussa_Day_2 {
     public static void main(String[] args) {
+
         System.out.println("Hi class");
+        System.out.println("GM. Have a good day");
     }
 }
