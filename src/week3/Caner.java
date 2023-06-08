df587246938ae9943cfe5e1860e344c357a714a3
CanerGungor <canercan-55@hotmail.com>
2023-06-08 11:11:34 -0400

    week3

--- src/week3/Caner.java
+++ src/week3/Caner.java
@@ -1,4 +1,35 @@
 package week3;
 
 public class Caner {
+    public static boolean isPrime(int num) {
+
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static int reverseNegative(int negative) {
+        int reverse = 0;
+
+        while (negative != 0) {
+
+            int digit = negative % 10;
+            reverse = reverse * 10 + digit;
+            negative /= 10;
+
+        }
+        return reverse;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("isPrime " + isPrime(23));
+        System.out.println(reverseNegative(-2485));
+
+    }
 }
+
