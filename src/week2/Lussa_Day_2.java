33ee2aba3a366e4bfbcf8fa10e68ec55636cc8bc
lussadecembre <lussa29@mail.com>
2023-06-09 08:45:10 -0400

    Gm week 3 task

--- src/week2/Lussa_Day_2.java
+++ src/week2/Lussa_Day_2.java
@@ -7,5 +7,26 @@ public class Lussa_Day_2 {
         System.out.println("GM. Have a good day");
         System.out.println("Good morning");
 
+
+
     }
+    public class OddOrEven {
+        //Week3 Task 1
+
+        //  public static void main(String[] args) {
+
+        int number = 100;
+
+        boolean isEven = number % 2 == 0;  // When divide a number by 2, if the remainder is zero, it means the number is even
+
+        // boolean isOdd = number % 2 != 0; // When divide a number by 2, if the remainder is NOT zero, it means the number is odd
+
+        boolean isOdd = !isEven; // if the number is not even, then it's odd
+    }
+
+
 }
+ //System.out.println(number + " is an even number: " +  isEven );
+            //System.out.println(number + " is an odd number: " + isOdd );
+
+
