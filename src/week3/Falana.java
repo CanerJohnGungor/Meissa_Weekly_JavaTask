7f174933e606fcb83278d34d7b23a4799503a122
flittle85 <flittle85@gmail.com>
2023-06-09 15:19:56 -0400

    completed day 1 second push

--- src/week3/Falana.java
+++ src/week3/Falana.java
@@ -3,7 +3,8 @@ package week3;
 public class Falana {
 
     public static void main(String[] args) {
-        System.out.println("HI"); 
+        System.out.println("HI");
+        System.out.println("Hi Lussa");
 
 
 
