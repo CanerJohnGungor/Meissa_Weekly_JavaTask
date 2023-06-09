6ce0333fff77f7570f249dbc95c8ebaca66849b0
lussadecembre <lussa29@mail.com>
2023-06-09 15:35:55 -0400

    Comment just all three weeks

--- src/week2/Lussa.java
+++ src/week2/Lussa.java
@@ -4,5 +4,6 @@ public class Lussa {
     public static void main(String[] args) {
         System.out.println("Hello class");
         System.out.println("Have a good day!!");
+        System.out.println("Hi, everyone.");
     }
 }
