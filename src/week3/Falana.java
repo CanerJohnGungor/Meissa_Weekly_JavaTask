b92c61c1755eef29a46935a3b33aad9104b3dcfa
flittle85 <flittle85@gmail.com>
2023-06-13 15:42:48 -0400

    completed day 2

--- src/week3/Falana.java
+++ src/week3/Falana.java
@@ -5,7 +5,7 @@ public class Falana {
     public static void main(String[] args) {
         System.out.println("HI");
         System.out.println("Hi Lussa");
-
+        System.out.println("Today is Tuesday");
 
 
     }
